e7662faadde3d715def471f0e4096bf2c3f7e67d
brickatyourfeet <brickatyourfeet@gmail.com>
2018-05-17 16:20:39 -0700

    add route files for users, profile, and posts

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
+
 const app = express()
 
 //configure database
@@ -13,6 +17,11 @@ mongoose
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('express express express'))
+
+//use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 const PORT = process.env.PORT || 5000
 
