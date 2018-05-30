9ce05f368fcfd4c6a683359cef10f651f0eb5041
brickatyourfeet <brickatyourfeet@gmail.com>
2018-05-30 16:36:13 -0700

    bcrypt compare pass for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,5 +46,32 @@ router.post('/register', (req, res)=> {
   })
 })
 
+// @route GET api/users/login
+// @desc login user / return token
+// @access Public
+router.post('/login', (req, res)=> {
+  const email = req.body.email
+  const password = req.body.password
+
+  //find user by email
+  User.findOne({ email })
+    .then(user => {
+      //check for user
+      if(!user){
+        return res.status(404).json({email: 'user not found'})
+      } 
+
+      //check PW
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(isMatch){
+            res.json({msg: 'success'})
+          } else {
+            return res.status(400).json({ password: 'incorrect password' })
+          }
+        })
+    })
+})
+
 
 module.exports = router
