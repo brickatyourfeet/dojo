7f8d72b450a1652d0315e6b7d4c35043687f3119
brickatyourfeet <brickatyourfeet@gmail.com>
2018-06-06 10:10:49 -0700

    start validation for users

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken')
 const keys = require('../../config/keys')
 const passport = require('passport')
 
+//load input validation
+const validateRegisterInput = require('../../validation/register')
+
 //load user model, which will have Mongoose methods attached
 const User = require('../../models/User')
 
@@ -18,10 +21,18 @@ router.get('/test', (req, res) => res.json({msg: "users test route"}))
 // @desc Register user
 // @access Public
 router.post('/register', (req, res)=> {
+  const { errors, isValid } = validateRegisterInput(req.body) 
+
+  //check validation
+  if(!isValid){
+    return res.status(400).json(errors)
+  }
+
   User.findOne({ email: req.body.email })
   .then(user => {
     if(user){
-      return res.status(400).json({email: 'email exists'})
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200',  //size
