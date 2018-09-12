1269225754b25b85255ecb18675228d782a48dac
brickatyourfeet <brickatyourfeet@gmail.com>
2018-09-11 21:56:13 -0700

    add delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -131,4 +131,60 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }),
     })
   })
 
+// @route POST api/posts/comment/:id
+// @desc add comment to post
+// @access Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), 
+(req, res) => {
+  const { errors, isValid } = validatePostInput(req.body)
+
+  if(!isValid){
+    //if errors, send 400 and error json
+    return res.status(400).json(errors)
+  }
+  Post.findById(req.params.id)
+  .then(post => {
+    const newComment = {
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    }
+
+    //add to comments array
+    post.comments.unshift(newComment)
+
+    post.save()
+    .then(post => res.json(post))
+  })
+  .catch(err => res.status(404).json({ postnotfound: err }))
+})
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc delete comment from post
+// @access Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), 
+(req, res) => {
+
+  Post.findById(req.params.id)
+  .then(post => {
+    //check that comment exists
+    if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+      return res.status(404).json({ nocomment: err })
+    }
+
+    //retrieve index to remove
+    const removeIndex = post.comments
+      .map(item => item._id.toString())
+      .indexOf(req.params.comment_id)
+
+    //splice out of array
+    post.comments.splice(removeIndex, 1)
+
+    post.save()
+    .then(post => res.json(post))
+  })
+  .catch(err => res.status(404).json({ postnotfound: err }))
+})
+
 module.exports = router
