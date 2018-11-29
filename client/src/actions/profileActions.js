33928a0752b371d243aa6530a463ede5307d3926
brickatyourfeet <brickatyourfeet@gmail.com>
2018-11-28 22:24:58 -0800

    add get profiles action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types'
+import { GET_PROFILE, GET_PROFILES, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER } from './types'
 
 // get curr profile
 export const getCurrentProfile = () => dispatch => {
@@ -86,6 +86,24 @@ export const deleteEducation = (id) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    )
+}
+
+// get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading())
+  axios.get('/api/profile/all')
+    .then(res => 
+        dispatch({
+          type: GET_PROFILES,
+          payload: res.data
+        })
+      )
+    .catch(err => 
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     )
 }
