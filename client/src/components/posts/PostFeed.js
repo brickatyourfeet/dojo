eea8c5c68da03b2a4021bbc3293706995e4f058f
brickatyourfeet <brickatyourfeet@gmail.com>
2018-12-03 13:07:08 -0800

    add delete user specific post

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -6,6 +6,7 @@ class PostFeed extends Component {
   render() {
     const { posts } = this.props
     return posts.map(post => <PostItem key={post._id} post={post} />)
+  }
 }
 
 PostFeed.propTypes = {
