57412728a7945864ffc75033639e7c1bb4bcb14b
brickatyourfeet <brickatyourfeet@gmail.com>
2018-12-06 22:08:19 -0800

    remove some comments

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -6,9 +6,8 @@ import { GET_ERRORS, SET_CURRENT_USER } from './types'
 
 
 export const registerUser = (userData, history) => dispatch => {
-  //adding dispatch like this is the same as returning dispatch
   axios.post('/api/users/register', userData)
-  .then(res => history.push('/login')) //redirect to login
+  .then(res => history.push('/login'))
   .catch(err => 
     dispatch({
       type: GET_ERRORS,
